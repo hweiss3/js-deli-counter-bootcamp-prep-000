2231eac42041d32ddf503a9ed1f29f724762f2be
Harrison Weiss <weiss.harrison@gmail.com>
2017-09-17 18:46:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function currentLine(katzDeli){
     return "The line is currently empty."
   } else {
     for (var i = 0; i < katzDeli.length; i++) {
-      katzDeli[i] = i+1. katzDeli[i]
     }
     return `The line is currently: ${i}. ${katzDeli[i]}`
   }
